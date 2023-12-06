817b3b7cb1d0f3d6ad7b1733268a353b9bfb8fef
lit.lina <angelina.work96@gmail.com>
2023-12-06 15:48:41 +0300

    Добавьте файлы проекта.

---
+++ GreatWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace GreatWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
