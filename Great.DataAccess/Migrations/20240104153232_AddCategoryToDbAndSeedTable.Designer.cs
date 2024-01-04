7f8115a24fbd3c7d83911f68ecbb17f2cae6beb3
lit.lina <angelina.work96@gmail.com>
2024-01-04 18:48:17 +0300

    Удалила прежние миграции в БД и создала новую в проекте "Great.DataAccess".

--- Great.DataAccess/Migrations/20240104153232_AddCategoryToDbAndSeedTable.Designer.cs
+++ Great.DataAccess/Migrations/20240104153232_AddCategoryToDbAndSeedTable.Designer.cs
@@ -11,8 +11,8 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Great.DataAccess.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    [Migration("20240103082109_SeedCategoryTable")]
-    partial class SeedCategoryTable
+    [Migration("20240104153232_AddCategoryToDbAndSeedTable")]
+    partial class AddCategoryToDbAndSeedTable
     {
         /// <inheritdoc />
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
@@ -24,7 +24,7 @@ namespace Great.DataAccess.Migrations
 
             SqlServerModelBuilderExtensions.UseIdentityColumns(modelBuilder);
 
-            modelBuilder.Entity("GreatWeb.Models.Category", b =>
+            modelBuilder.Entity("Great.Models.Category", b =>
                 {
                     b.Property<int>("Id")
                         .ValueGeneratedOnAdd()
@@ -37,7 +37,8 @@ namespace Great.DataAccess.Migrations
 
                     b.Property<string>("Name")
                         .IsRequired()
-                        .HasColumnType("nvarchar(max)");
+                        .HasMaxLength(30)
+                        .HasColumnType("nvarchar(30)");
 
                     b.HasKey("Id");
 
