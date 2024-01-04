93a672fcdf6aee0071d94ec83f1948313ae152e0
lit.lina <angelina.work96@gmail.com>
2024-01-04 13:04:19 +0300

    Вкладка "Category" - реализовала вспылающие окна с сообщениями при добавлении, редактировании и удалении категории.

--- GreatWeb/Controllers/CategoryController.cs
+++ GreatWeb/Controllers/CategoryController.cs
@@ -32,6 +32,7 @@ namespace GreatWeb.Controllers
 			{
 				_db.Categories.Add(obj);
 				_db.SaveChanges();
+				TempData["success"] = "Category create successfully!";
 				return RedirectToAction("Index");
 			}
 			return View();
@@ -60,9 +61,37 @@ namespace GreatWeb.Controllers
 			{
 				_db.Categories.Update(obj);
 				_db.SaveChanges();
+				TempData["success"] = "Category updated successfully!";
 				return RedirectToAction("Index");
 			}
 			return View();
+		}
+
+		public IActionResult Delete(int? id)
+		{
+			if (id == null || id == 0)
+			{
+				return NotFound();
+			}
+			Category? categoryFromDb = _db.Categories.Find(id);
+
+			if (categoryFromDb == null)
+			{
+				return NotFound();
+			}
+			return View(categoryFromDb);
+		}
+		[HttpPost, ActionName("Delete")]
+		public IActionResult DeletePOST(int? id)
+		{
+			Category? obj = _db.Categories.Find(id); if (obj == null)
+			{
+				return NotFound();
+			}
+			_db.Categories.Remove(obj);
+			_db.SaveChanges();
+			TempData["success"] = "Category deleted successfully!";
+			return RedirectToAction("Index");
 		}
 	}
 }
