3915e9ced5fc9f308fc465c70a2d098e4f6edf79
lit.lina <angelina.work96@gmail.com>
2024-01-08 13:42:09 +0300

    Создала области Admin и Customer. Реализовала выпадающий список в хедере сайта.

--- GreatWeb/Areas/Customer/Controllers/HomeController.cs
+++ GreatWeb/Areas/Customer/Controllers/HomeController.cs
@@ -2,9 +2,10 @@ using GreatFilms.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 
-namespace GreatFilms.Controllers
+namespace GreatFilmsWeb.Areas.Customer.Controllers
 {
-    public class HomeController : Controller
+	[Area("Customer")]
+	public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
 
