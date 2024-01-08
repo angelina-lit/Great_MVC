e4ff459eb4f97ae4db74c71167c7032149f92445
lit.lina <angelina.work96@gmail.com>
2024-01-08 13:00:18 +0300

    Реализовала паттерн UnitOfWork.

--- Great.DataAccess/Repository/CategoryRepository.cs
+++ Great.DataAccess/Repository/CategoryRepository.cs
@@ -11,18 +11,13 @@ using System.Threading.Tasks;
 
 namespace GreatFilms.DataAccess.Repository
 {
-	public class CategoryRepository : Repository<Category>, ICategoryRepository
+    public class CategoryRepository : Repository<Category>, ICategoryRepository
 	{
 		private ApplicationDbContext _db;
         public CategoryRepository(ApplicationDbContext db) : base(db)
         {
             _db = db;
         }
-
-        public void Save()
-		{
-			_db.SaveChanges();
-		}
 
 		public void Update(Category obj)
 		{
