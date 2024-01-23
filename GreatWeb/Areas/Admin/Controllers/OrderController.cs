b42cf69897d9cb1b2366e9fb9e51bf963d0c8a67
lit.lina <angelina.work96@gmail.com>
2024-01-23 17:53:27 +0300

    На странице Manage Order при изменении деталей заказа реализовала обновление и сохранение данных, а так же реализовала возможность изменения этих данных только для пользователей с ролью Admin и Employee. Реализовала отображение всех заказов (любого пользователя) только для пользователей с ролью Admin и Employee, для отстальных - отображение только их заказов.

--- GreatWeb/Areas/Admin/Controllers/OrderController.cs
+++ GreatWeb/Areas/Admin/Controllers/OrderController.cs
@@ -2,9 +2,11 @@
 using GreatFilms.Models;
 using GreatFilms.Models.ViewModels;
 using GreatFilms.Utility;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
+using System.Security.Claims;
 
 namespace GreatFilmsWeb.Areas.Admin.Controllers
 {
@@ -12,6 +14,9 @@ namespace GreatFilmsWeb.Areas.Admin.Controllers
 	public class OrderController : Controller
 	{
 		private readonly IUnitOfWork _unitOfWork;
+
+		[BindProperty]
+		public OrderVM OrderVM { get; set; }
 
 		public OrderController(IUnitOfWork unitOfWork)
 		{
@@ -24,21 +29,62 @@ namespace GreatFilmsWeb.Areas.Admin.Controllers
 
 		public IActionResult Details(int orderId)
 		{
-			OrderVM orderVM = new()
+			OrderVM = new()
 			{
 				OrderHeader = _unitOfWork.OrderHeader.Get(u => u.Id == orderId, includeProperties: "ApplicationUser"),
 				OrderDetail = _unitOfWork.OrderDetail.GetAll(u => u.OrderHeaderId == orderId, includeProperties: "Product")
 			};
 
-			return View(orderVM);
+			return View(OrderVM);
 		}
+
+		[HttpPost]
+		[Authorize(Roles = SD.Role_Admin + "," + SD.Role_Employee)]
+		public IActionResult UpdateOrderDetails(int orderId)
+		{
+			var orderHeaderFromDb = _unitOfWork.OrderHeader.Get(u => u.Id == OrderVM.OrderHeader.Id);
+
+			orderHeaderFromDb.Name = OrderVM.OrderHeader.Name;
+			orderHeaderFromDb.PhoneNumber = OrderVM.OrderHeader.PhoneNumber;
+			orderHeaderFromDb.StreetAddress = OrderVM.OrderHeader.StreetAddress;
+			orderHeaderFromDb.City = OrderVM.OrderHeader.City;
+			orderHeaderFromDb.State = OrderVM.OrderHeader.State;
+			orderHeaderFromDb.PostalCode = OrderVM.OrderHeader.PostalCode;
+			if (!string.IsNullOrEmpty(OrderVM.OrderHeader.Carrier))
+			{
+				orderHeaderFromDb.Carrier = OrderVM.OrderHeader.Carrier;
+			}
+			if (!string.IsNullOrEmpty(OrderVM.OrderHeader.TrackingNumber))
+			{
+				orderHeaderFromDb.TrackingNumber = OrderVM.OrderHeader.TrackingNumber;
+			}
+			_unitOfWork.OrderHeader.Update(orderHeaderFromDb);
+			_unitOfWork.Save();
+
+			TempData["Success"] = "Order Details Updated Successfully.";
+
+			return RedirectToAction(nameof(Details), new { orderId = orderHeaderFromDb.Id });
+		}
+
 
 		#region API CALLS
 
 		[HttpGet]
 		public IActionResult GetAll(string status)
 		{
-			IEnumerable<OrderHeader> objOrderHeaders = _unitOfWork.OrderHeader.GetAll(includeProperties: "ApplicationUser").ToList();
+			IEnumerable<OrderHeader> objOrderHeaders;
+
+			if (User.IsInRole(SD.Role_Admin) || User.IsInRole(SD.Role_Employee))
+			{
+				objOrderHeaders = _unitOfWork.OrderHeader.GetAll(includeProperties: "ApplicationUser").ToList();
+			}
+			else
+			{
+				var claimsIdentity = (ClaimsIdentity)User.Identity;
+				var userId = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier).Value;
+
+				objOrderHeaders = _unitOfWork.OrderHeader.GetAll(u => u.ApplicationUserId == userId, includeProperties: "ApplicationUser");
+			}
 
 			switch (status)
 			{
