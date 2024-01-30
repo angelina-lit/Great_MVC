196dd2f7cd72f725592c74f515ba7bac10deb89f
lit.lina <angelina.work96@gmail.com>
2024-01-30 19:12:36 +0300

    Страница Manage User Role, реализовала логику для обновления роли пользователя при нажатии кнопки "Update".

--- GreatWeb/Areas/Admin/Controllers/UserController.cs
+++ GreatWeb/Areas/Admin/Controllers/UserController.cs
@@ -4,9 +4,11 @@ using GreatFilms.Models;
 using GreatFilms.Models.ViewModels;
 using GreatFilms.Utility;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
+using System.Collections;
 
 namespace GreatFilmsWeb.Areas.Admin.Controllers
 {
@@ -15,10 +17,11 @@ namespace GreatFilmsWeb.Areas.Admin.Controllers
 	public class UserController : Controller
 	{
 		private readonly ApplicationDbContext _db;
-		private readonly IWebHostEnvironment _webHostEnvironment;
-		public UserController(ApplicationDbContext db)
+		private readonly UserManager<IdentityUser> _userManager;
+		public UserController(ApplicationDbContext db, UserManager<IdentityUser> userManager)
 		{
 			_db = db;
+			_userManager = userManager;
 		}
 		public IActionResult Index()
 		{
@@ -47,6 +50,34 @@ namespace GreatFilmsWeb.Areas.Admin.Controllers
 			RoleVM.ApplicationUser.Role = _db.Roles.FirstOrDefault(u => u.Id == RoleId).Name;
 
 			return View(RoleVM);
+		}
+
+		[HttpPost]
+		public IActionResult RoleManagment(RoleManagmentVM roleManagmentVM)
+		{
+			string RoleId = _db.UserRoles.FirstOrDefault(u => u.UserId == roleManagmentVM.ApplicationUser.Id).RoleId;
+			string oldRole = _db.Roles.FirstOrDefault(u => u.Id == RoleId).Name;
+
+			if (!(roleManagmentVM.ApplicationUser.Role == oldRole))
+			{
+				//a role was updated
+				ApplicationUser applicationUser = _db.applicationUser.FirstOrDefault(u => u.Id == roleManagmentVM.ApplicationUser.Id);
+				if(roleManagmentVM.ApplicationUser.Role == SD.Role_Company)
+				{
+					applicationUser.CompanyId = roleManagmentVM.ApplicationUser.CompanyId;
+				}
+				if(oldRole == SD.Role_Company)
+				{
+					applicationUser.CompanyId = null;
+				}
+
+				_db.SaveChanges();
+
+				_userManager.RemoveFromRoleAsync(applicationUser, oldRole).GetAwaiter().GetResult();
+				_userManager.AddToRoleAsync(applicationUser, roleManagmentVM.ApplicationUser.Role).GetAwaiter().GetResult();
+			}
+
+			return RedirectToAction("Index");
 		}
 
 		#region API CALLS
